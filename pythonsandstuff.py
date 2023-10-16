d5385830d11e97bb3d1ea7720bef0e98b9151af1
Contreras <jbcontreras@cps.k12.il.us>
2023-10-16 16:29:18 -0500

    inital commit

--- pythonsandstuff.py
+++ pythonsandstuff.py
@@ -1,16 +1,18 @@
 from flask import Flask
 import json
+app = __name__
 @app.route("/")
 def home():
     # passes stuff to the move and genre data to 'home.html' i think 
     pass
 
-@app.route
+@app.route("/movie/<title>")
 def movie_detail(title):
     #retrieve stuff and pass selected movie details to 'movie_detail.html'
     pass
 
-
+@app.route("/genre/<genre_name>")
 def genre_page(genre_name):
     # retrieve and pass all movies of the selected genre to genre_page.html
     pass
+
